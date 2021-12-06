64fc8ad0b38d9a17d794b93cf78f4908636c38d5
manuelRestrep0 <94997816+manuelRestrep0@users.noreply.github.com>
2021-12-05 22:32:09 -0500

    Update main.java

--- Reto_3/src/main/java/com/mycompany/reto_3/main.java
+++ Reto_3/src/main/java/com/mycompany/reto_3/main.java
@@ -47,11 +47,13 @@ public class main {
     static void ejercicio_1()
     {
         System.out.println("Ingrese el numero total de notas que va a ingresar");
+        
         Scanner input = new Scanner(System.in);
         if(input.hasNextInt())
         {
             int n = input.nextInt();
             float mayor=0,menor=0,promedio=0,desviacion=0;
+            double sumatoria = 0;
             float[] notas = new float[n];
             for(int i=0;i<=n;i++)
             {
@@ -60,20 +62,36 @@ public class main {
                 if(input.hasNextFloat())
                 {
                     notas[i] = input.nextFloat();
-                    promedio = promedio + notas[i];
-                    if(mayor!=0 && menor!=0)
+                    if(notas[i]>=0 && notas[i]<=5)
                     {
-                        if(mayor<notas[i])
+                        promedio = promedio + notas[i];
+                        if(i==0)
+                        {
                             mayor=notas[i];
-                        if(menor>notas[i])
                             menor=notas[i];
-                     }
+                        }
+                        if(mayor!=0 && menor!=0)
+                        {
+                            if(mayor<notas[i])
+                                mayor=notas[i];
+                            if(menor>notas[i])
+                                menor=notas[i];
+                        }
+                    }
+                    
                 }   
             }
-        }
-        
-        
-        
+            for(int i=0;i<n;i++)
+            {
+                sumatoria = sumatoria + (notas[i]-promedio)*(notas[i]-promedio);  
+            }
+            sumatoria = (Math.sqrt(sumatoria))/(n-1);
+            promedio = promedio/n;  
+            System.out.println("La mayor nota es:"+mayor);
+            System.out.println("La menor nota es:"+menor);
+            System.err.println("El promedio de las notas es:"+promedio);
+            System.err.println("La desviación estandar del conjunto de las nota es:"+sumatoria);
+        }  
     }
     static void ejercicio_2()
     {
