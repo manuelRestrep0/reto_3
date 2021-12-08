46f40193ede8dd86f6111c22e781857e9809eae0
manuelRestrep0 <94997816+manuelRestrep0@users.noreply.github.com>
2021-12-07 23:47:24 -0500

    Update main.java

--- Reto_3/src/main/java/com/mycompany/reto_3/main.java
+++ Reto_3/src/main/java/com/mycompany/reto_3/main.java
@@ -57,77 +57,81 @@ public class main {
     }
     static void ejercicio_1()
     {
-        System.out.println("Ingrese el numero total de notas que va a ingresar");
+        System.out.println("Ingrese las notas separadas por cosas:");
         Scanner input = new Scanner(System.in);
-        if(input.hasNextInt())
+        String notas = input.nextLine();
+        String[] arregloNotas = notas.split(",");
+        int i=0;
+        float mayor=0,menor=0,promedio=0;
+        double sumatoria = 0;
+        float[] arregloNotasFloat = new float[arregloNotas.length];
+        while(i!=(arregloNotas.length))
         {
-            int n = input.nextInt();
-            float mayor=0,menor=0,promedio=0,desviacion=0;
-            double sumatoria = 0;
-            float[] notas = new float[n];
-            for(int i=0;i<n;i++)
+            arregloNotasFloat[i] = Float.parseFloat(arregloNotas[i]);
+            if(arregloNotasFloat[i] >=0 && arregloNotasFloat[i]<=5)
             {
-                System.out.println("Ingrese la nota");
-                input = new Scanner(System.in);
-                if(input.hasNextFloat())
-                {
-                    notas[i] = input.nextFloat();
-                    if(notas[i]>=0 && notas[i]<=5)
-                    {
-                        promedio = promedio + notas[i];
+                  promedio = promedio + arregloNotasFloat[i];
                         if(i==0)
                         {
-                            mayor=notas[i];
-                            menor=notas[i];
+                            mayor=arregloNotasFloat[i];
+                            menor=arregloNotasFloat[i];
+                            i++;
                             continue;
                         }
                         if(mayor!=0 && menor!=0)
                         {
-                            if(mayor<notas[i])
-                                mayor=notas[i];
-                            if(menor>notas[i])
-                                menor=notas[i];
+                            if(mayor<arregloNotasFloat[i])
+                                mayor=arregloNotasFloat[i];
+                            if(menor>arregloNotasFloat[i])
+                                menor=arregloNotasFloat[i];
+                            i++;
                         }
-                    }           
-                }   
-            }
-            System.out.println(Arrays.toString(notas));
-            promedio = promedio/n; 
-            for(int i=0;i<n;i++)
+            } 
+        }
+        promedio = promedio/i;
+            for(i=0;i<arregloNotasFloat.length;i++)
             {
-                sumatoria = sumatoria + Math.pow((notas[i]-promedio), 2);   //no da
-            }
-            sumatoria = (Math.sqrt(sumatoria))/(n-1);
+                sumatoria = sumatoria + Math.pow((arregloNotasFloat[i]-promedio), 2);
+            }        
+            sumatoria = (Math.sqrt(sumatoria))/(i-1);
             System.out.println("La mayor nota es:"+mayor);
             System.out.println("La menor nota es:"+menor);
             System.err.println("El promedio de las notas es:"+promedio);
             System.err.println("La desviación estandar del conjunto de las nota es:"+sumatoria);
-        }  
     }
     static void ejercicio_2()
     {
+        //cambiar la entrada de los numeros
         System.out.println("Ingrese el numero equivalente al tamano de los vectores");
-        Scanner input = new Scanner(System.in);
-        if(input.hasNextInt())
-        {
-            int n = input.nextInt();
-            double multiplicacion = 0;
-            float[] vector_1 = new float[n];
-            float[] vector_2 = new float[n];
-            for(int i=0;i<n;i++)
-            {
-                System.out.println("Ingrese el valor numero "+(i+1)+" del vector numero 1");
-                vector_1[i] = recibir_valor();
-                System.out.println("Ingrese el valor numero "+(i+1)+" del vector numero 2");
-                vector_2[i] = recibir_valor();
-                multiplicacion = multiplicacion + (vector_1[i]*vector_2[i]);
-            }
-            System.out.println("La multiplicacion escalar de los dos vectores es: "+multiplicacion);
-        }
-        
+        double multiplicacion = 0;
+        System.out.println("Ingrese el valor numero ");
+        float[] vector_1 = recibir_valor();
+        System.out.println("Ingrese el valor numero ");
+        float[] vector_2 = recibir_valor();
+        for(int i=0;i<vector_1.length;i++)
+           {
+            multiplicacion = multiplicacion + (vector_1[i]*vector_2[i]);
+           }
+        System.out.println("La multiplicacion escalar de los dos vectores es: "+multiplicacion);   
     }
     static void ejercicio_3()
     {
+        /*
+        System.out.println("ingrese el vector 1");
+        double[] vector_1 = recibir_valor_double();
+        System.out.println("ingrese el vector 2");
+        double[] vector_2 = recibir_valor_double();
+        double[][] vector_final = new double[vector_1.length][vector_2.length];
+        if(vector_1.length==vector_2.length)
+        {
+            for(int i=0;i<vector_1.length;i++)
+            {
+                for(int j=0;j<vector_2.length;j++)
+                {
+                    vector_final[i][j] = 
+                }
+            }
+        }*/
         System.out.println("Ingrese el numero n");
         Scanner input = new Scanner(System.in);
         if(input.hasNextInt())
@@ -156,21 +160,23 @@ public class main {
                 }
             }
             
-        }
-        
+        }    
     }
-    static float recibir_valor()
+    static float[] recibir_valor()
     {
         while(true)
         {
+            int i = 0;
             Scanner input = new Scanner(System.in);
-            if(input.hasNextFloat())
+            String notas = input.nextLine();
+            String[] arregloNotas = notas.split(",");
+            float[] arregloNotasFloat = new float[arregloNotas.length];
+            while(i!=arregloNotasFloat.length)
             {
-                float valor = input.nextFloat();
-                return valor;
+                arregloNotasFloat[i] = Float.parseFloat(arregloNotas[i]); 
+                i++;
             }
-            else
-               System.out.println("Ingrese un valor valido");
+            return arregloNotasFloat;
         }      
     }
     static double recibir_valor_double()
@@ -187,5 +193,6 @@ public class main {
             else
                System.out.println("Ingrese un valor valido");
         }      
+        
     }
 }
